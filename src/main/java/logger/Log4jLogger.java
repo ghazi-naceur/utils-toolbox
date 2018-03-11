6c7e64ff3bc41b530c2e721b3542a798a4f2330e
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2018-03-11 01:34:34 +0100

    Basic Logger

--- src/main/java/logger/Log4jLogger.java
+++ src/main/java/logger/Log4jLogger.java
@@ -4,10 +4,10 @@ import org.apache.log4j.Logger;
 
 public class Log4jLogger{
 
-    static Logger log = Logger.getLogger(Log4jLogger.class.getName());
+    static Logger LOGGER = Logger.getLogger(Log4jLogger.class.getName());
 
     public static void main(String[] args){
-        log.debug("Hello this is a debug message");
-        log.info("Hello this is an info message");
+        LOGGER.debug("Hello this is a debug message");
+        LOGGER.info("Hello this is an info message");
     }
 }
